90dcf7547711091f8d9fcadb7ea76a3187ae3ac2
tim-graupe <tgraupe90@gmail.com>
2023-05-29 18:00:15 -0400

    try to fix port error

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
