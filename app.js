e309273e0c5dd9efaec6775a2eddf3d5d6dec51b
tim-graupe <tgraupe90@gmail.com>
2023-05-31 18:47:08 -0400

    port change to 7748

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7748;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
