73778cbc0bb7f5a40170a4da7be01f00de5110e5
tim-graupe <tgraupe90@gmail.com>
2023-05-31 17:26:24 -0400

    port adjustment

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-const port = 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
