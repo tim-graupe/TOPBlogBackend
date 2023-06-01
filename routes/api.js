d9d38603ce9bc10ac58f8e37347fe99a93d60dcc
tim-graupe <tgraupe90@gmail.com>
2023-06-01 18:27:08 -0400

    remove date

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,12 @@
 var express = require("express");
 var router = express.Router();
+const cors = require("cors");
 const new_entry_controller = require("../controllers/newEntryController");
 const get_entries_controller = require("../controllers/getEntriesController");
 /* GET home page. */
 
 router.get("/", (req, res) => {
   return res.send("GET HTTP method on user resource");
-});
-
-// router.post("/", (req, res) => {
-//   return res.send("POST HTTP method on user resource");
-// });
-
-router.put("/", (req, res) => {
-  return res.send("PUT HTTP method on user resource");
-});
-
-router.delete("/", (req, res) => {
-  return res.send("DELETE HTTP method on user resource");
 });
 
 //new entry
