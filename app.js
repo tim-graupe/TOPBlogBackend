d0026ec415dfb29c059d1e3a5b89356312a55fef
tim-graupe <tgraupe90@gmail.com>
2023-06-01 22:48:48 -0400

    remove proces..env.dev_db_url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const corsOptions = {
 //mongoose
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI || process.env.dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
