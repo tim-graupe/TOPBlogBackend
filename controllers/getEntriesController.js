70e2f077d91c326eba75268c850d9d8a3924f74f
tim-graupe <tgraupe90@gmail.com>
2023-06-04 19:21:55 -0400

    change to findByIdAndDelete

--- controllers/getEntriesController.js
+++ controllers/getEntriesController.js
@@ -33,7 +33,8 @@ exports.editEntry = async function (req, res, next) {
 //delete entry
 exports.deleteEntry = async (req, res, next) => {
   const entry = await Entry.find({ _id: req.params.id });
-  entry.findByIdAndRemove(entry, function (err, docs) {
+  //switched from findByIdAndRemove
+  entry.findByIdAndDelete(entry, function (err, docs) {
     if (err) {
       console.log(err);
       res.redirect("/");
