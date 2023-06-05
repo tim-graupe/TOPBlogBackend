7e26529693b1ed0b74a96fe4ff8d3888e5d512b5
tim-graupe <tgraupe90@gmail.com>
2023-06-04 22:41:04 -0400

    back to adding app.delete

--- routes/api.js
+++ routes/api.js
@@ -26,6 +26,6 @@ router.get("/entries/:id", get_entries_controller.singleEntry);
 router.put("/entries/:id", get_entries_controller.editEntry);
 
 //delete entry
-router.delete("/entries/:id", cors(), get_entries_controller.deleteEntry);
+router.delete("/entries/:id", get_entries_controller.deleteEntry);
 
 module.exports = router;
