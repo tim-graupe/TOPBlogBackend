f8bab3ba670ae6e24ccb4595e954e0e811873784
tim-graupe <tgraupe90@gmail.com>
2023-06-06 13:34:57 -0400

    typo

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -7,7 +7,7 @@ exports.sign_up = async (req, res, next) => {
       username: req.body.username,
       password: req.body.password,
       adminCode: req.body.adminCode,
-      isAdmnin: adminCode === process.env.passcode ? true : false,
+      isAdmin: adminCode === process.env.passcode ? true : false,
     });
     const result = await user.save();
   } catch (err) {
