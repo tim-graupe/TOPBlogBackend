077230a19121ba8aa45bf7c99fedd62055f1438f
tim-graupe <tgraupe90@gmail.com>
2023-06-05 20:19:15 -0400

    change to Date.now

--- models/newEntryModel.js
+++ models/newEntryModel.js
@@ -11,7 +11,7 @@ const EntrySchema = new Schema({
     maxLength: 1000,
   },
   //   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  date_posted: { type: Date, default: new Date() },
+  date_posted: { type: Date, default: Date.now },
   isPublished: { type: Boolean, default: false },
   replies: { type: Array },
 });
