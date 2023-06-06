9deebfb647e7be11067366985cfc36568e7c4c7c
tim-graupe <tgraupe90@gmail.com>
2023-06-06 14:30:43 -0400

    try to fix internal error

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,16 +1,16 @@
 const { body, validationResult } = require("express-validator");
 const User = require("../models/newUserModel");
 
-exports.sign_up = async (req, res, next) => {
+exports.sign_up_controller = async (req, res, next) => {
   try {
     const user = new User({
       username: req.body.username,
       password: req.body.password,
       adminCode: req.body.adminCode,
-      isAdmin: adminCode === process.env.passcode ? true : false,
+      isAdmin: req.body.adminCode === process.env.passcode ? true : false,
     });
     const result = await user.save();
   } catch (err) {
-    return next(err);
+    console.log("Error was found: " + err);
   }
 };
