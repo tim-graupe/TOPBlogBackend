68eb2374edb6703526fbb668fabe147b8b50c3ec
tim-graupe <tgraupe90@gmail.com>
2023-06-08 18:39:43 -0400

    attempt signup fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const { body, validationResult } = require("express-validator");
 const User = require("../models/newUserModel");
+const bcrypt = require("bcrypt");
 
 exports.sign_up_controller = async (req, res, next) => [
   body("username", "Username required!")
@@ -19,8 +20,7 @@ exports.sign_up_controller = async (req, res, next) => [
     const user = new User({
       username: req.body.username,
       password: req.body.password,
-      admin: req.body.admin,
-      member: req.body.member ? true : false,
+      adminCode: req.body.adminCode,
     });
     bcrypt.hash(req.body.password, 10, (err, hashedPwd) => {
       if (err) return next(err);
@@ -37,5 +37,3 @@ exports.login_get = (req, res) => {
   // If user is already logged in, redirect them to the homepage
   if (res.locals.currentUser) return res.redirect("/");
 };
-
-exports.login_controller = async (req, res, next) => {};
