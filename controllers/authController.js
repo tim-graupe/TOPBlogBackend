e97f0125c241b1f71ef9a01c7475b77626afc7db
tim-graupe <tgraupe90@gmail.com>
2023-06-08 19:51:14 -0400

    change back to sign_up

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,40 +4,13 @@ const bcrypt = require("bcrypt");
 
 exports.sign_up_controller = async (req, res, next) => {
   try {
-    const errors = validationResult(req);
-    let desiredName = await User.findOne({ username: req.body.username });
-    if (desiredName) {
-      return res.render("signup", { userError: "User Already Exists" });
-    }
-    if (!errors.isEmpty() && !desiredName) {
-      res.render("signup", {
-        username: req.body.username,
-        password: req.body.password,
-        admin: req.body.admin,
-        member: req.body.member ? true : false,
-      });
-      return;
-    }
     const user = new User({
       username: req.body.username,
       password: req.body.password,
-      admin: req.body.admin,
-      member: req.body.member ? true : false,
+      adminCode: req.body.adminCode,
     });
-    bcrypt.hash(req.body.password, 10, (err, hashedPwd) => {
-      if (err) return next(err);
-      user.password = hashedPwd;
-      user.save((err) => {
-        if (err) return next(err);
-        res.redirect("/");
-      });
-    });
-    // user.save((err) => {
-    //   if (err) {
-    //     return next(err);
-    //   }
-    //   res.redirect("/");
-    // });
+    const result = await user.save();
+    res.redirect("/");
   } catch (err) {
     return next(err);
   }
