f6c1709d79dd3c2a4ea3818f33bac84170ef3b1b
tim-graupe <tgraupe90@gmail.com>
2023-06-12 18:46:18 -0400

    add app.post back

--- app.js
+++ app.js
@@ -116,6 +116,10 @@ app.put("/entries/:id", cors(), function (req, res, next) {
   res.json({ msg: "cors enabled, for all origins!" });
 });
 
+app.post("/log-in", cors(), function (req, res, next) {
+  res.json({ msg: "cors enabled, for login origins!" });
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
