c4f3519404e5c917159e4b69b67335f5985108b4
tim-graupe <tgraupe90@gmail.com>
2023-06-12 18:05:13 -0400

    remove passport from file

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,33 +3,6 @@ const User = require("../models/newUserModel");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const app = express();
-app.use(express.json());
-
-passport.use(
-  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-    User.findOne({ email: email }, (err, user) => {
-      if (err) return done(err);
-      if (!user) return done(null, false, { message: "Incorrect email" });
-
-      bcrypt.compare(password, user.password, (err, isMatch) => {
-        if (err) throw err;
-        if (isMatch) return done(null, user);
-        else return done(null, false, { message: "Incorrect password" });
-      });
-    });
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
 
 exports.sign_up_controller = [
   body("username", "Username required!")
