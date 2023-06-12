281f9f4a77da8e4dc91a8f39f0f2d69f770184b0
tim-graupe <tgraupe90@gmail.com>
2023-06-12 18:49:58 -0400

    more fiddling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,46 @@
 const { body, validationResult } = require("express-validator");
 const User = require("../models/newUserModel");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+
+//passport
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 exports.sign_up_controller = [
   body("username", "Username required!")
