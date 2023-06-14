278102e8f9f5569c305f471d2ff94457a028713c
tim-graupe <tgraupe90@gmail.com>
2023-06-14 19:12:03 -0400

    add passport.authenticate('local'),

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,11 @@ router.get("/", (req, res) => {
 });
 
 //login
-router.post("/log-in", auth_controller.login_post);
+router.post(
+  "/log-in",
+  passport.authenticate("local"),
+  auth_controller.login_post
+);
 
 //new entry
 router.get("/new_entry", (req, res) => {
