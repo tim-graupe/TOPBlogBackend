99e2ba42bf6c4c80a3bee39c4fc0072a6c83de25
tim-graupe <tgraupe90@gmail.com>
2023-06-14 17:54:02 -0400

    remove duplicate post, add passport.use

--- app.js
+++ app.js
@@ -49,27 +49,29 @@ app.use(express.urlencoded({ extended: false }));
 
 //passport
 
-new LocalStrategy(function (username, password, done) {
-  User.findOne({ username: username }, function (err, user) {
-    if (err) {
-      return done(err);
-    }
-    if (!user) {
-      return done(null, false);
-    }
-    bcrypt.compare(password, user.password, (err, res) => {
-      if (res) {
-        // passwords match! log user in
-        return done(null, user);
-      } else {
-        // passwords do not match!
-        return done(null, false, { message: "Incorrect password" });
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
       }
+      if (!user) {
+        return done(null, false);
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+  
+      return done(null, user);
     });
-
-    return done(null, user);
   });
-});
+)
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
@@ -83,14 +85,6 @@ passport.deserializeUser(async function (id, done) {
     done(err);
   }
 });
-
-app.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/",
-  })
-);
 
 app.use(function (req, res, next) {
   res.locals.isLoggedIn = req.isAuthenticated();
