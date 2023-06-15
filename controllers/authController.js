123acface56611e6147bedb07bf21b5a989db666
tim-graupe <tgraupe90@gmail.com>
2023-06-15 18:50:29 -0400

    add   req.session.username = req.body.username

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,8 +54,11 @@ exports.login_post = (req, res, next) => {
     failureRedirect: "/sign_up",
     // passReqToCallback: true,
   });
+
   res.json({
     auth: req.isAuthenticated(),
     currentUser: res.locals.currentUser,
   });
+  req.session.username = req.body.username;
+  res.end();
 };
