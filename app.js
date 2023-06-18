8dd96dc56f7f734f1f7952d3b51a31483e3533ef
tim-graupe <tgraupe90@gmail.com>
2023-06-18 17:48:02 -0400

    add app.use(passport.authenticate('session'))

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(passport.authenticate("session"));
 //cors
 const corsOptions = {
   origin: [
