11b4cb2489822451766c4b4682616440dfa8cada
tim-graupe <tgraupe90@gmail.com>
2023-06-18 16:34:07 -0400

    add bodyparser

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const router = require("./routes/api");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(express.json());
 app.use(express.bodyParser());
