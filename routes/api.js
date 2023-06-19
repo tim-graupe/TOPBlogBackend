405da864291206f99e73d52987e2f14692c85d78
tim-graupe <tgraupe90@gmail.com>
2023-06-19 13:02:12 -0400

    replace cors()

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
 
 //login
 //login
-router.post("/log-in", auth_controller.login_post);
+router.post("/log-in", cors(), auth_controller.login_post);
 router.get("/log-in", function (req, res) {
   if (req.user) {
     res.json(req.user);
